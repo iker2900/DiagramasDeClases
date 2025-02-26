e87a8679e90cde5d281eb40123ce45b8e828450f
Iker <iker29992@gmail.com>
2025-02-26 19:17:59 +0100

    Ejercicio4 hecho

---
+++ Ejercicio4/Product.java
@@ -0,0 +1,7 @@
+class Product {
+    private String name;
+    private boolean checked;
+
+    public Product(String name) {}
+    public void checkItem() {}
+}
---
+++ Ejercicio4/ShoppingList.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class ShoppingList {
+    private List<Product> items;
+
+    public void addItem(Product product) {}
+    public void removeItem(Product product) {}
+    public List<Product> viewList() {}
+}
